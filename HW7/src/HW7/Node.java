fe5e2b94e160b7ea36ccef9640846bfb5b1306a3
magmanex <timecrisis1995@live.com>
2016-12-06 01:36:33 +0700

    Create HW7 + original code from AJ

---
+++ HW7/src/HW7/Node.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HW7;
+
+/**
+ *
+ * @author Asuka
+ */
+public class Node {
+    double price;
+    int investorID;
+    int amount;
+    long timestamp; // Heap.push() is the only function that modify this variable
+    boolean isMinHeap;
+    public Node(double price, int investorID, int amount, boolean isMinHeap){
+        this.price = price;
+        this.investorID = investorID;
+        this.amount = amount;
+        this.isMinHeap = isMinHeap;
+    }
+
+    // return true if Priority(thisNode) > Priority(rightNode) and vice versa
+    // minHeap: the lower the price, the higher the priority and vice versa
+    // maxHeap: the lower the price, the lower the priority and vice versa
+    //
+    // You may create your own function if you do not understand my code
+    public boolean compare(Node rightNode){
+        if (this.price == rightNode.price) {
+            return false; // FIX THIS
+        }else{
+            if (isMinHeap){
+                return false; // FIX THIS
+            }else{
+                return false; // FIX THIS
+            }
+        }
+    }
+}
+
---
+++ HW7/src/HW7/Stock.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HW7;
+
+/**
+ *
+ * @author Asuka
+ */
+public class Stock {
+    int id;
+    String name;
+    double currentPrice;
+    public Stock(int id, String name, double price){
+        this.id = id;
+        this.name = name;
+        this.currentPrice = price;
+    }
+}
+
